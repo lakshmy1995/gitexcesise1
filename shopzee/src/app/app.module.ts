ecefaafced74d68704f0e8840b2797b3e1420360
Lakshmy <Lakshmy25rahul@gmail.com>
2023-04-19 13:29:12 +0530

    Revert "fifth change occur"
    This reverts commit 7598aa8df326b3a124892623f89b1d0ee59b920c.

--- shopzee/src/app/app.module.ts
+++ shopzee/src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
+<<<<<<< HEAD
 import { StudentComponent } from './student/student.component';
 import { DetailComponent } from './detail/detail.component';
+=======
+>>>>>>> parent of 7598aa8 (fifth change occur)
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+<<<<<<< HEAD
     BodyComponent,
     StudentComponent,
     DetailComponent
+=======
+    BodyComponent
+>>>>>>> parent of 7598aa8 (fifth change occur)
   ],
   imports: [
     BrowserModule,
